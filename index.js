0453c75dff6f4382a07737af97b1d6ffa21dc1e4
Nicholas van de Walle <nicholas@niceguys.co>
2015-04-17 17:19:13 -0700

    Initialization works

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var path = require('path');
-var parser = require('nomnom');
+var fs = require('fs');
+var parser     = require('nomnom');
 var fileExists = require('file-exists');
 
 var yoapi = require('yo-api');
@@ -10,84 +10,93 @@ var yoclirc = getUserHome() + '/.yoclirc';
 
 var config;
 var apiKey;
+var commandName = 'yocli';
 
-if (!fileExists(yoclirc)) {
+if (fileExists(yoclirc)) {
+  config = JSON.parse(fs.readFileSync(yoclirc));
+  run(config.apiKey, config.commandName);
+} else {
   console.log('Please setup yoclirc');
   // run init prompt
-  setup(yoclirc);
-} else {
-  config = JSON.parse(fs.readFileSync(yoclirc));
-  apiKey = config.apiKey;
+  setup(yoclirc, function(err, config) {
+    if (err) throw err;
+    fs.writeFileSync(yoclirc, JSON.stringify(config));
+    run(config.apiKey, config.commandName);
+  });
 }
 
-var keyData = {
-  abbr: 'k',
-  metavar: 'API_KEY',
-  help: 'use a specific api key'
-};
+function run(apiKey, commandName) {
+  var keyData = {
+    abbr: 'k',
+    metavar: 'API_KEY',
+    help: 'use a specific api key'
+  };
 
-var urlData = {
-  abbr: 'u',
-  metavar: 'URL',
-  help: 'URL link or image to send'
-};
+  var urlData = {
+    abbr: 'u',
+    metavar: 'URL',
+    help: 'URL link or image to send'
+  };
+  // TODO Change this based on loading json and checking for not manual symlink
+  parser.script(commandName);
 
-// TODO Change this based on loading json and checking for not manual symlink
-parser.script('yocli');
+  parser.command('yo')
+    .option('key', keyData)
+    .option('url', urlData)
+    .callback(function(opts) {
+      var key = opts.key || apiKey;
+      yoapi(key, opts[1], opts.url)
+        .then(function() {
+          console.log('Yo sent to ' + opts[1]);
+        })
+        .catch(function(err) {
+          console.log(err.message);
+          process.exit(1);
+        });
+    });
 
-parser.command('yo')
-  .option('key', keyData)
-  .option('url', urlData)
-  .callback(function(opts) {
-    var key = opts.key || apiKey;
-    yoapi(key, opts[1], opts.url)
-      .then(function() {
-        console.log('Yo sent to ' + opts[1]);
-      })
-      .catch(function(err) {
-        console.log(err.message);
-        process.exit(1);
+  parser.command('all')
+    .option('key', keyData)
+    .option('url', urlData)
+    .callback(function(opts) {
+      var key = opts.key || apiKey;
+      yoapi(key, 'all', opts.url)
+        .then(function() {
+          console.log('Yo sent to all');
+        })
+        .catch(function(err) {
+          console.log(err.message);
+          process.exit(1);
+        });
+    })
+    .help('Yo all Yo subscribers');
+
+  parser.command('subs')
+    .option('key', keyData)
+    .callback(function(opts) {
+      var key = opts.key || apiKey;
+      yoapi.subs(key)
+        .then(function(responseBody) {
+          console.log('Yo have ' + responseBody.count + ' subscribers');
+        })
+        .catch(function(err) {
+          console.log(err.message);
+          process.exit(1);
+        })
+    })
+    .help('Count Yo subscribers');
+
+  parser.command('init')
+    .callback(function(opts) {
+      setup(yoclirc, function(err, config) {
+        fs.writeFileSync(yoclirc, JSON.stringify(config));
+        process.exit(0);
       });
-  });
+    })
+    .help('Rerun initial setup');
 
-parser.command('all')
-  .option('key', keyData)
-  .option('url', urlData)
-  .callback(function(opts) {
-    var key = opts.key || apiKey;
-    yoapi(key, 'all', opts.url)
-      .then(function() {
-        console.log('Yo sent to all');
-      })
-      .catch(function(err) {
-        console.log(err.message);
-        process.exit(1);
-      });
-  })
-  .help('Yo all yo subscribers');
-
-parser.command('subs')
-  .option('key', keyData)
-  .callback(function(opts) {
-    var key = opts.key || apiKey;
-    yoapi.subs(key)
-      .then(function(responseBody) {
-        console.log('Yo have ' + responseBody.count + ' subscribers');
-      })
-      .catch(function(err) {
-        console.log(err.message);
-        process.exit(1);
-      })
-  })
-  .help('Get a count of yo subscribers');
-
-parser.command('init')
-  .callback(function(opts) {
-
-  })
-  .help('Rerun initial setup');
-
-parser.parse();
+  parser.parse();
+}
 
 function getUserHome() {
   return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
--- setup/index.js
+++ setup/index.js
@@ -1,3 +1,10 @@
-var fs = require('fs');
 var prompt = require('prompt');
 
+function setup(location, callback) {
+  console.log('Hey there, let\'s setup Yo CLI');
+  console.warn('Warning: '.red + 'YoCLI stores auth data unencrypted in ~/.yoclirc');
+  prompt.start();
+  prompt.get(['apiKey', 'commandName'], callback);
+}
+
+module.exports = setup;
